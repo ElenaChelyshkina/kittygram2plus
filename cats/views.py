b0d588cff2f6b0f023ff2eb968572d6a1a8a5999
ElenaChelyshkina <len.vorontsovaa@gmail.com>
2024-08-24 15:18:45 +0300

    init

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,77 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+
+from rest_framework.throttling import AnonRateThrottle, ScopedRateThrottle
 
 from .models import Achievement, Cat, User
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
+from .permissions import OwnerOrReadOnly, ReadOnly
+
+from .throttling import WorkingHoursRateThrottle
+
+from rest_framework.pagination import PageNumberPagination
+
+from .pagination import CatsPagination
+
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import filters
+
+
+
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    # Устанавливаем разрешение
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    permission_classes = (OwnerOrReadOnly,)
+    #throttle_classes = (AnonRateThrottle,)  # Подключили класс AnonRateThrottle
+
+    # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
+    # Если он вернёт False - все запросы будут отклонены
+    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
+    # Для любых пользователей установим кастомный лимит 1 запрос в минуту
+    throttle_scope = 'low_request' 
+    # Пагинацию можно установить для отдельного view-класса (для Generics или Viewsets)
+    # pagination_class = PageNumberPagination 
+
+    # Вот он наш собственный класс пагинации с page_size=20
+    # pagination_class = CatsPagination
+
+    # Указываем фильтрующий бэкенд DjangoFilterBackend
+    # Из библиотеки django-filter
+    # Для сортировки можно подключить встроенный бэкенд OrderingFilter
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    )
+    # Временно отключим пагинацию на уровне вьюсета, 
+    # так будет удобнее настраивать фильтрацию
+    pagination_class = None
+    # Фильтровать будем по полям color и birth_year модели Cat
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name', 'achievements__name')
+    # поля для сортировки перечисляются в атрибуте ordering_fields
+    ordering_fields = ('name', 'birth_year')
+    # Упорядочим выдачу по умолчанию по году рождения
+    ordering = ('birth_year',)
+
+
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        # Если в GET-запросе требуется получить информацию об объекте
+        if self.action == 'retrieve':
+            # Вернём обновлённый перечень используемых пермишенов
+            return (ReadOnly(),)
+        # Для остальных ситуаций оставим текущий перечень пермишенов без изменений
+        return super().get_permissions() 
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'djoser',
     'cats.apps.CatsConfig',
 ]
@@ -134,6 +135,24 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        # Не будем подключать класс AnonRateThrottle глобально.
+        # Подключим его только в тех view-классах или вьюсетах,
+        # где надо установить лимиты для анонимов
+        #'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.ScopedRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        # Но сами лимиты установим, и они будут доступны из всего кода проекта
+        'user': '10000/day',  # Лимит для UserRateThrottle
+        'anon': '1000/day',  # Лимит для AnonRateThrottle
+        # Имена (ключи) для scope придумывает разработчик, 
+        # в меру собственной фантазии
+        'low_request': '5/minute',
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3,
 }
 
 SIMPLE_JWT = {
